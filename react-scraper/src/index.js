0192d668ecce76fa5a148d48eb5d3715b9e2e97b
paradigm1984 <danblanco1990@gmail.com>
2017-08-20 10:35:10 -0400

    initial commit

--- react-scraper/src/index.js
+++ react-scraper/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/App';
-import registerServiceWorker from './extra/registerServiceWorker';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
